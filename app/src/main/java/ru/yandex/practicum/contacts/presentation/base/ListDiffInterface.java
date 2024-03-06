324bd0d7709622f4a365ba197bf7692f0943eb9a
volkovdimaya <volkov.dimaya@yandex.ru>
2024-03-06 17:28:47 +0500

    добавил в интерфейс ListDiffInterface что в методы null не может поподать

--- app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ListDiffInterface.java
@@ -1,14 +1,16 @@
 package ru.yandex.practicum.contacts.presentation.base;
 
+import androidx.annotation.NonNull;
+
 import ru.yandex.practicum.contacts.presentation.filter.model.FilterContactTypeUi;
 import ru.yandex.practicum.contacts.presentation.main.ContactUi;
 
 public interface ListDiffInterface<T extends ListDiffInterface> {
 
 
-    public boolean theSameAs(T t);
+    public boolean theSameAs(@NonNull T t);
 
 
 
-    public boolean equals(Object o);
+    public boolean equals(@NonNull  Object o);
 }
